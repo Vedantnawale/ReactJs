813c064349de16c72b1b2dff32b7a9edee41a3f0
Vedantnawale <vedantnawale6@gmail.com>
2023-11-12 10:09:57 -0500

    Creating project with basicreact and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from './Chai';
+
+function App() {
+  return (
+    <>
+    <Chai />
+    {/* <h1>Chai aur React | Vedant :- I learn two projects in ReactJS. I Write first time code in ReactJS</h1> */}
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+function App() {
+  
+  return (
+    // <h1>chai aur react</h1> separate run hot nahi ekch element return hote.
+    // ekt div nahi t empty use karaych
+    <>  
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
