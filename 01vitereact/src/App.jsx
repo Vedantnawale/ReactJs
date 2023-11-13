057bc7b8c29630b6b776791fc871f7bd3bf24ac4
Vedantnawale <vedantnawale6@gmail.com>
2023-11-13 08:54:40 -0500

    cutom element

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./Chai"
 function App() {
-  
+  const username = "chai aur code"
   return (
     // <h1>chai aur react</h1> separate run hot nahi ekch element return hote.
     // ekt div nahi t empty use karaych
     <>  
     <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <p>test para</p>
     </>
   )
