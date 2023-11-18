126f51e35fc0f7e551ff1f2d139b353788124d71
Vedantnawale <vedantnawale6@gmail.com>
2023-11-18 23:05:17 +0530

    include command for run

--- 01basicreact/src/Chai.js
+++ 01basicreact/src/Chai.js
@@ -1,6 +1,11 @@
-function Chai(){
+function Chai() {
     return (
-        <h2>chai in react</h2>
+        <h2>chai in react
+            npx create-react-app my-app {/* command for run */}
+            cd my-app
+            npm start
+        </h2>
+
     )
 }
 
