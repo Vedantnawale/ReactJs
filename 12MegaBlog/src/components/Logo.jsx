52cf95e28fb777a20f3e223a45b4f7854bdafe9d
Vedantnawale <vedantnawale6@gmail.com>
2023-12-05 19:05:46 +0530

    logo add

--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
     return (
-        <div>Logo</div>
+        <div>
+            <img 
+            width={36}
+            className=' rounded-full  py-0'
+            src="/assets/images/Nawale.png" 
+            alt="logo" 
+             />
+        </div>
     )
 }
 
